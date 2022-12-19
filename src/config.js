b4006387fd687756556e40b6634a4e790a46e973
Carlos Urdaneta <curdanet@hotmail.com>
2022-12-19 19:36:50 -0300

    Add console list all environment

--- src/config.js
+++ src/config.js
@@ -1,9 +1,12 @@
 const timestamp = Date.now(); 
 console.log(new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(timestamp));
+console.log(process.env)
+console.log('-----------------------------------------------------------------------')
 const bg= {        
     host: process.env.REACT_APP_BE_HOST,
     port: process.env.REACT_APP_BE_PORT,
     bkend_path:`http://${process.env.REACT_APP_BE_HOST}:${process.env.REACT_APP_BE_PORT}`        
 }
+console.log(new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(timestamp));
 console.log('Paso por config.js :',bg);
 module.exports={bg}
