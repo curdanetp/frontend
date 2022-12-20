c6a2d47bff15f95c2430e40e04719f29a3967f65
Carlos Urdaneta <curdanet@hotmail.com>
2022-12-20 01:04:49 -0300

    Fixed Call api

--- src/config.js
+++ src/config.js
@@ -4,8 +4,8 @@ console.log(window.location.hostname);
 console.log('-----------------------------------------------------------------------')
 const bg= {        
     host: `http://${window.location.hostname}/api`,
-    port: 4000,
-    bkend_path:`http://${window.location.hostname}:4000/api`        
+    port: 80,
+    bkend_path:`http://${window.location.hostname}/api`        
 }
 console.log(new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(timestamp));
 console.log('Paso por config.js :',bg.bkend_path);
