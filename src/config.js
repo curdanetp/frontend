779faf06661d23d356283e56635900c7cccf3556
Carlos Urdaneta <curdanet@hotmail.com>
2022-12-20 16:12:19 -0300

    Fixing :

--- src/config.js
+++ src/config.js
@@ -3,9 +3,9 @@ console.log(new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',
 console.log(window.location.hostname);
 console.log('-----------------------------------------------------------------------')
 const bg= {        
-    host: `http://${window.location.hostname}/api`,
-    port: 80,
-    bkend_path:`http://${window.location.hostname}/api`        
+    host: `http://${window.location.hostname}`,
+    port: 30004,
+    bkend_path:`http://${window.location.hostname}:30004/api`        
 }
 console.log(new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(timestamp));
 console.log('Paso por config.js :',bg.bkend_path);
