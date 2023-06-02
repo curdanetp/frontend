38b1d525e315556eb781e8aea47ce9019b6ff045
Carlos Urdaneta <curdanet@hotmail.com>
2023-06-01 22:10:10 -0400

    Testing

--- src/config.js
+++ src/config.js
@@ -2,10 +2,11 @@ const timestamp = Date.now();
 console.log(new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(timestamp));
 console.log(window.location.hostname);
 console.log('-----------------------------------------------------------------------')
+const portserv=80;
 const bg= {        
     host: `http://${window.location.hostname}`,
-    port: 80,
-    bkend_path:`http://${window.location.hostname}:80`        
+    port: portserv,
+    bkend_path:`http://${window.location.hostname}:${portserv}`        
 }
 console.log(new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(timestamp));
 console.log('Paso por config.js :',bg.bkend_path);
